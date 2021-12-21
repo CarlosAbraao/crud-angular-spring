50f41350b52e9cbd0e8c5a3ba781c5d97bee507f
Carlos Abraao <carloschavesnd@hotmail.com>
2021-12-21 12:57:33 -0300

    configurando modulos e criando um estilo para o projeto

--- angular_spring/src/app/app-routing.module.ts
+++ angular_spring/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'initial'
+  },
+
+
+    {
+      path: 'initial',
+      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    }
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
